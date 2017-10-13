bff4076a016b159d80c8bbb290e570a11985139a
Brook Elgie <specialbrew@gmail.com>
2017-10-13 10:10:47 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Tools for working with Data Packages and CKAN
+
+[![Travis](https://img.shields.io/travis/frictionlessdata/ckan-datapackage-tools/master.svg)](https://travis-ci.org/frictionlessdata/ckan-datapackage-tools)
+[![Coveralls](http://img.shields.io/coveralls/frictionlessdata/ckan-datapackage-tools/master.svg)](https://coveralls.io/r/frictionlessdata/ckan-datapackage-tools?branch=master)
+[![PyPi](https://img.shields.io/pypi/v/ckan-datapackage-tools.svg)](https://pypi.python.org/pypi/ckan-datapackage-tools)
+[![SemVer](https://img.shields.io/badge/versions-SemVer-brightgreen.svg)](http://semver.org/)
+[![Gitter](https://img.shields.io/gitter/room/frictionlessdata/chat.svg)](https://gitter.im/frictionlessdata/chat)
+
+## Install
+
+```
+# clone the repo and install it with pip
+
+git clone https://github.com/frictionlessdata/ckan-datapackage-tools.git
+pip install -e .
+```
+
+## Usage
---
+++ setup.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+
+import os
+import io
+from setuptools import setup, find_packages
+
+
+# Helpers
+def read(*paths):
+    """Read a text file."""
+    basedir = os.path.dirname(__file__)
+    fullpath = os.path.join(basedir, *paths)
+    contents = io.open(fullpath, encoding='utf-8').read().strip()
+    return contents
+
+
+# Prepare
+PACKAGE = 'ckan_datapackage_tools'
+NAME = PACKAGE.replace('_', '-')
+INSTALL_REQUIRES = [
+]
+TESTS_REQUIRE = [
+    'pylama',
+    'tox',
+]
+README = read('README.md')
+VERSION = read(PACKAGE, 'VERSION')
+PACKAGES = find_packages(exclude=['examples', 'tests'])
+
+
+# Run
+setup(
+    name=NAME,
+    version=VERSION,
+    packages=PACKAGES,
+    include_package_data=True,
+    install_requires=INSTALL_REQUIRES,
+    tests_require=TESTS_REQUIRE,
+    extras_require={'develop': TESTS_REQUIRE},
+    zip_safe=False,
+    long_description=README,
+    description='{{ DESCRIPTION }}',
+    author='Open Knowledge International',
+    url='https://github.com/frictionlessdata/ckan_datapackage_tools',
+    license='MIT',
+    keywords=[
+        'data',
+        'ckan'
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
