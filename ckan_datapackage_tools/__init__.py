0e7fe3329b6f5351c1a5f65fb8fb7e99b1e43b28
Sebastien Lavoie <sebastien.lavoie@datopian.com>
2020-06-14 14:17:56 -0500

    [tests][l]: Add test_package to convert package
      * Convert CKAN package JSON to Frictionless package

--- ckan_datapackage_tools/__init__.py
+++ ckan_datapackage_tools/__init__.py
@@ -1,15 +1,64 @@
-class CKANToFrictionless(object):
+class CKANToFrictionless:
 
     resource_mapping = {
-      'size': 'bytes',
-      'mimetype': 'mediatype',
-      'url': 'path'
+        'size': 'bytes',
+        'mimetype': 'mediatype',
+        'url': 'path'
+    }
+
+    package_mapping = {
+        'tags': 'keywords',  # this is flattened and simplified
+        'url': 'homepage'
     }
 
     def resource(self, ckandict):
         outdict = dict(ckandict)
-        for k,v in self.resource_mapping.items():
+        for k, v in self.resource_mapping.items():
             if k in outdict:
                 outdict[v] = outdict[k]
                 del outdict[k]
+        return outdict
+
+    def package(self, ckandict):
+        outdict = dict(ckandict)
+
+        # Remap keys `url` and `tags` to Frictionless
+        # format `path` and `keywords`
+        for k, v in self.package_mapping.items():
+            if k in ckandict and k == 'url':
+                outdict[v] = ckandict[k]
+                del outdict[k]
+            if k in ckandict and k == 'tags':
+                outdict[v] = []
+                for tag in ckandict[k]:
+                    outdict[v].append(tag['name'])
+                del outdict[k]
+
+        # Reformat expected output for some keys in package
+        outdict['title'] = outdict['title'].replace(' ', '-')
+        outdict['name'] = outdict['name'].replace('-', '_')
+
+        # Reformat resources inside package
+        for res in outdict['resources']:
+            # Remove keys not needed
+            del res['package_id']
+            del res['position']
+
+            # Reformat expected output for some keys in resource
+            res['format'] = res['format'].lower()
+            res['name'] = res['name'].lower().replace(' ', '-')
+
+            # Remap differences from CKAN to Frictionless resource
+            for k, v in self.resource_mapping.items():
+                if k in res:
+                    res[v] = res[k]
+                    del res[k]
+
+                    # Cast CKAN resource size to int
+                    if k == 'size':
+                        if not res[v]:  # receives `null`
+                            res[v] = 0
+                        else:
+                            res[v] = int(res[v])
+
         return outdict
--- tests/test_ckan_to_frictionless_mapping.py
+++ tests/test_ckan_to_frictionless_mapping.py
@@ -1,14 +1,23 @@
-import unittest
 import json
 
 import ckan_datapackage_tools
 
 
-def test_ckan_to_frictionless_json_conversion_resource():
+def test_resource():
     inpath = 'tests/fixtures/ckan_resource.json'
     exppath = 'tests/fixtures/frictionless_resource.json'
     converter = ckan_datapackage_tools.CKANToFrictionless()
     indict = json.load(open(inpath))
     exp = json.load(open(exppath))
     out = converter.resource(indict)
+    assert out == exp
+
+
+def test_package():
+    inpath = 'tests/fixtures/ckan_package.json'
+    exppath = 'tests/fixtures/frictionless_package.json'
+    converter = ckan_datapackage_tools.CKANToFrictionless()
+    indict = json.load(open(inpath))
+    exp = json.load(open(exppath))
+    out = converter.package(indict)
     assert out == exp
