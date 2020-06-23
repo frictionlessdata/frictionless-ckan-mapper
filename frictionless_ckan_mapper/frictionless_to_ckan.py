e0d3ec484fc4a578891ff9a659af8e0778640678
Sebastien Lavoie <sebastien.lavoie@datopian.com>
2020-06-23 08:45:34 -0500

    [docstring][s]: Improve documentation for Frictionless=>CKAN conversion
    * Remove TODOs not needed anymore.
    * Update function docstrings to reflect the desired behavior of the
      algorithm.

--- frictionless_ckan_mapper/frictionless_to_ckan.py
+++ frictionless_ckan_mapper/frictionless_to_ckan.py
@@ -8,24 +8,41 @@ except AttributeError:  # Testing against Python 2
 
 class FrictionlessToCKAN:
 
-    # TODO: How do we map the required CKAN `package_id`?
-    # TODO: In CKAN, `private` is another required field:
-    #       set default value to False?
-
     resource_mapping = {
         'bytes': 'size',
         'mediatype': 'mimetype',
         'path': 'url'
     }
 
-    # TODO: Do we passthrough all the FD properties not found in CKAN?
-    # `data`: This replaces Resource.path if it is not specified.
-    #         However, it is not an URL. Should it just passthrough?
-    # `homepage`: Does this passthrough? What if we also have FD Resource.path
-    #             => we can't map both to CKAN Resource.url
-    # `licenses`: How do we map multiple licenses from Frictionless to CKAN?
-    resource_keys_to_remove = [
-        '',
+    package_mapping = {
+        'description': 'notes',
+        'homepage': 'url',
+        'keywords': 'tags'
+    }
+
+    # Any key not in this list is passed as is inside "extras".
+    # Further processing will happen for possible matchings, e.g.
+    # contributor <=> author
+    CKAN_package_keys = [
+        "author",
+        "author_email",
+        "groups",
+        "license_id",
+        "maintainer",
+        "maintainer_email",
+        "name",
+        "notes",
+        "owner_org",
+        "private",
+        "relationships_as_object",
+        "relationships_as_subject",
+        "resources",
+        "state",
+        "tags",
+        "title",
+        "type",
+        "url",
+        "version"
     ]
 
     def resource(self, fddict):
@@ -33,11 +50,10 @@ class FrictionlessToCKAN:
 
         # TODO: (the following is inaccurate)
 
-        1. Remove unneeded keys (are there any keys to remove?)
-        2. Convert extras: "dict" to "list of dict".
-        3. Map keys from Frictionless to CKAN (and reformat if needed).
-        4. Remove keys with null values.
-        5. Apply special formatting (if any) for key fields e.g. slugify
+        1. Convert extras: "dict" to "list of dict".
+        2. Map keys from Frictionless to CKAN (and reformat if needed).
+        3. Apply special formatting (if any) for key fields e.g. slugify.
+        4. Map anything not in CKAN inside the "extras" key.
         '''
         pass
 
@@ -46,10 +62,8 @@ class FrictionlessToCKAN:
 
         # TODO: (the following is inaccurate)
 
-        1. Copy extras across.  # Is this true?
-        2. Map keys from Frictionless to CKAN (and reformat if needed).
-        3. Remove keys with null values (CKAN has a lot of null valued keys).
-        4. Remove unneeded keys.
-        5. Apply special formatting for key fields.
+        1. Map keys from Frictionless to CKAN (and reformat if needed).
+        2. Apply special formatting (if any) for key fields.
+        3. Copy extras across inside the "extras" key.
         '''
         pass
